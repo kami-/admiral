a42be4501573ff4d21a2ebf8cbb643c0eb9a91ea
Laszlo Major <laszlo.major@outlook.com>
2014-03-28 20:49:03 +0100

    Renamed fields.

--- admiral/unit_templates.h
+++ admiral/unit_templates.h
@@ -4,18 +4,18 @@ class AdmiralUnitTemplates {
     class Base {
         side=SIDE_WEST;
         infantry[] = {};
-        technicals[] = {};
+        technical[] = {};
         armour[] = {};
-        crew[] = {};
+        crewmen[] = {};
         pilots[] = {};
     };
 
     class TK_Army_Woodland : Base {
         side=SIDE_EAST;
         infantry[] = {"TK_Soldier_EP1"};
-        technicals[] = {"UAZ_MG_TK_EP1"};
+        technical[] = {"UAZ_MG_TK_EP1"};
         armour[] = {"T72_TK_EP1"};
-        crew[] = {"TK_Soldier_EP1"};
-        pilots[] = {"TK_Soldier_EP1"};
+        crewmen[] = {"TK_Soldier_Crew_EP1"};
+        pilots[] = {"TK_Soldier_Pilot_EP1"};
     };
 };
